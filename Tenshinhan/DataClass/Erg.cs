2cd0ad2b0a5fbedcad6b5c112a7ab631be2b9c6e
mogami <atsuyatsuchikawa@gmail.com>
2021-11-02 02:19:32 +0900

    プロジェクト ファイルを追加します。

---
+++ Tenshinhan/DataClass/Erg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tenshinhan.DataClass
+{
+    class Erg
+    {
+        public string title { get; set; }
+        public string maker { get; set; }
+        public string janCode { get; set; }
+        public string gechuyaID { get; set; }
+    }
+}
---
+++ Tenshinhan/DataClass/ErgSave.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tenshinhan.DataClass
+{
+    class ErgSave: Erg
+    {
+        DateTime lastUpdateTime { get; set; }
+        string fileId { get; set; }
+    }
+}
