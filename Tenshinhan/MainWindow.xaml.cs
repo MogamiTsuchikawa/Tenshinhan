260f6de39c6ec7bf2b07f9f2a130b64bda4b2293
最上土川 <atsuyatsuchikawa@gmail.com>
2021-11-05 04:10:56 +0900

    メインウインドウのデザインを作成

--- Tenshinhan/MainWindow.xaml.cs
+++ Tenshinhan/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MicrosoftGraph;
 
 namespace Tenshinhan
 {
@@ -23,6 +24,80 @@ namespace Tenshinhan
         public MainWindow()
         {
             InitializeComponent();
+        }
+        
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            /*var res = await App.MicrosoftGraph.Auth(this);
+            AuthBtn.Content = res ? "認証成功" : "認証失敗";
+            var client = await App.MicrosoftGraph.GetClient();
+            var items = await client.Me.Drive.Root.Children.Request().GetAsync();
+            foreach(var item in items)
+            {
+                if(item.Folder == null)
+                {
+                    ResultBox.Text += $"{item.Name} \n";
+                }
+                else
+                {
+                    ResultBox.Text += $"{item.Name} (フォルダ) \n";
+                }
+            }*/
+        }
+
+        private void AddNewGameMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            AddErgWindow addErgWindow = new(AddErgWindow.Kind.AddNewGame);
+            addErgWindow.ShowDialog();
+        }
+
+        private void AddGameMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            AddErgWindow addErgWindow = new(AddErgWindow.Kind.AddGame);
+            addErgWindow.ShowDialog();
+        }
+
+        private async void OneDriveLoginMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            //MicrosoftLogin
+            var res = await App.MicrosoftGraph.Auth(this);
+            if (res)
+            {
+                OneDriveLoginMenuItem.Header = "ログイン中";
+                OneDriveLoginMenuItem.IsEnabled = false;
+                OneDriveLogoutMenuItem.IsEnabled = true;
+                MainWindowWindow.Title = "チャーハン天津飯(Microsoftログイン済)";
+            }
+        }
+
+        private async void OneDriveLogoutMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            //MicrosoftLogout
+            var res = await App.MicrosoftGraph.Logout();
+            if (res)
+            {
+                OneDriveLoginMenuItem.Header = "ログイン";
+                OneDriveLoginMenuItem.IsEnabled = true;
+                OneDriveLogoutMenuItem.IsEnabled = false;
+                MainWindowWindow.Title = "天津飯";
+            }
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var res = await App.MicrosoftGraph.SilentAuth();
+            if (res)
+            {
+                OneDriveLoginMenuItem.Header = "ログイン中";
+                OneDriveLoginMenuItem.IsEnabled = false;
+                MainWindowWindow.Title = "チャーハン天津飯(Microsoftログイン済)";
+            }
+            else
+            {
+                OneDriveLogoutMenuItem.IsEnabled = false;
+                MainWindowWindow.Title = "天津飯";
+            }
         }
     }
 }
