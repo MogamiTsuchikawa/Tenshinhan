a16e757319cb4691501b73947ac19ca44c9385f0
最上土川 <atsuyatsuchikawa@gmail.com>
2021-11-05 04:11:30 +0900

    LocalErgSaveクラスを追加

--- Tenshinhan/DataClass/ErgSave.cs
+++ Tenshinhan/DataClass/ErgSave.cs
@@ -8,7 +8,19 @@ namespace Tenshinhan.DataClass
 {
     class ErgSave: Erg
     {
-        DateTime lastUpdateTime { get; set; }
-        string fileId { get; set; }
+        public DateTime lastUpdateTime { get; set; }
+        public string oneDriveFileId { get; set; }
+        public string saveDataPath { get; set; }
+        public enum SaveDataPathKind
+        {
+            DocumentsFolder,AppDataFolder, AppExistFolder 
+        }
+        public SaveDataPathKind saveDataPathKind { get; set; }
+        
+    }
+
+    class LocalErgSave: ErgSave
+    {
+        public string appPath { get; set; }
     }
 }
