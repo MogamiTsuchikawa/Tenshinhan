d0df13c6bec6f2c23f8101378b389ac1489dcc50
最上土川 <atsuyatsuchikawa@gmail.com>
2021-11-05 04:11:06 +0900

    まだファイル作っただけ

---
+++ Tenshinhan/ErgFileManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrosoftGraph;
+
+namespace Tenshinhan
+{
+    public class ErgFileManager
+    {
+        private MicrosoftGraph.MicrosoftGraph graph;
+        private bool isReady = false;
+        public ErgFileManager(MicrosoftGraph.MicrosoftGraph graph)
+        {
+            this.graph = graph;
+
+        }
+
+    }
+}
