be30e1bc54e0f9d21e640e26560b48818be63583
mogami <atsuyatsuchikawa@gmail.com>
2021-11-05 08:16:14 +0900

    EXEのアイコンを表示できるように

--- Tenshinhan/ErgListItem.xaml.cs
+++ Tenshinhan/ErgListItem.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -22,14 +24,23 @@ namespace Tenshinhan
     public partial class ErgListItem : UserControl
     {
         public LocalErgSave erg { get; private set; }
+        private Action<LocalErgSave> onClick;
 
-        public ErgListItem(LocalErgSave erg)
+        public ErgListItem(LocalErgSave erg, Action<LocalErgSave> onClick)
         {
+            this.onClick = onClick;
             this.erg = erg;
             InitializeComponent();
             TitleText.Content = erg.title;
             MakerText.Content = erg.maker;
             LastUpdateTimeText.Content = $"最終 {erg.lastUpdateTime.ToString()}";
+            Icon icon = Icon.ExtractAssociatedIcon(erg.appPath);
+            ErgImg.Source = Imaging.CreateBitmapSourceFromHIcon(icon.Handle, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+        }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            onClick?.Invoke(erg);
         }
     }
 }
